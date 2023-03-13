74007bc26e1c1c370c5ddfa1609fd152a85ee968
Ryan <pinkstonr54@gmail.com>
2023-03-13 00:15:45 -0500

    wrote the necessairy variables/prompts to begin the function

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,31 @@
 // Assignment code here
-
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var bigAlphabet = alphabet.toUpperCase();
+var numbers = "0123456789";
+var specialCharacters = "!@$%^&*";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
+var numberDesired = prompt("please select number of characters desired (1-126)");
+var alphabetQ = prompt("do you want lower case letters? yes/no");
+var bigAplhabetQ = prompt("do you want upper case letters? yes/no");
+var numbersQ = prompt("do you want numbers? yes/no");
+var specialCharactersQ = prompt("do you want special characters? yes/no");
+var numberDesiredNumeric = Number(numberDesired);
+
+function writePassword () {
+  
+}
+
 generateBtn.addEventListener("click", writePassword);
