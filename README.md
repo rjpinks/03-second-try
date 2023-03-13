a8d2b8bfb3424c5371c3d2ba9eba21a67fb0c492
Ryan <pinkstonr54@gmail.com>
2023-03-13 01:26:31 -0500

    added the selection verification that i forgot

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Password Generator Starter Code
+# Password Generating App
+
+## Description
+
+I could not figure out how to write a proper code generator, so I was remotivated to take a second look at it after having a better understanding of Javascript and troubleshooting methods. I built it to prove to myself that I have learned a great deal since my first attempt. The app generates a random password for the user using the credentials that they desire. I learned that coding is definitely a marathon and not a sprint. Just looking at the time required for me to complete the second attempt compared to the first attempt illuminate this very strongly. I spent less than two hours writing the code in my second attempt, and I spent an embarassingly more amount of time despite not getting the code to work the first time.
+
+## Installation
+
+The only thing necessary would be a web browser to interpret the code.
+
+## Usage
+
+Follow the instructions given by the prompts then click the generate password button to create a copy-pastable password. If an answer besides "yes" or "Yes" is given it will default to not using that character selection.
+
+![alt text]()
+![alt text]()
--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,9 @@ var questionList = ""
 //html elements
 var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
+var wrapperEl = document.querySelector(".wrapper");
 
-//prompts that collect user data
+//prompts that collect user data and associated if statements that append the choice to the page
 var numberDesired = prompt("please select number of characters desired (8-126)");
 var alphabetQ = prompt("do you want lower case letters? yes/no");
 var bigAlphabetQ = prompt("do you want upper case letters? yes/no");
@@ -17,6 +18,59 @@ var numbersQ = prompt("do you want numbers? yes/no");
 var specialCharactersQ = prompt("do you want special characters? yes/no");
 var numberDesiredNumeric = Number(numberDesired);
 
+//funtions used in the app
+function selectionAppender () {
+  if (numberDesiredNumeric > 7 && numberDesiredNumeric < 127) {
+    var newEl = document.createElement("p");
+    var newText = document.createTextNode(numberDesired + " characters desired");
+    newEl.appendChild(newText);
+    wrapperEl.appendChild(newEl);
+  }
+  if (alphabetQ === "yes" || alphabetQ === "Yes") {
+    var newEl = document.createElement("p");
+    var newText = document.createTextNode("Lowercase selected");
+    newEl.appendChild(newText);
+    wrapperEl.appendChild(newEl);
+  } else {
+    var newEl = document.createElement("p");
+    var newText = document.createTextNode("Lowercase not selected");
+    newEl.appendChild(newText);
+    wrapperEl.appendChild(newEl);
+  }
+  if (bigAlphabetQ === "yes" || bigAlphabetQ === "Yes") {
+    var newEl = document.createElement("p");
+    var newText = document.createTextNode("Uppercase selected");
+    newEl.appendChild(newText);
+    wrapperEl.appendChild(newEl);
+  } else {
+    var newEl = document.createElement("p");
+    var newText = document.createTextNode("Uppercase not selected");
+    newEl.appendChild(newText);
+    wrapperEl.appendChild(newEl);
+  }
+  if (numbersQ === "yes" || numbersQ === "Yes") {
+    var newEl = document.createElement("p");
+    var newText = document.createTextNode("Numbers selected");
+    newEl.appendChild(newText);
+    wrapperEl.appendChild(newEl);
+  } else {
+    var newEl = document.createElement("p");
+    var newText = document.createTextNode("Numbers not selected");
+    newEl.appendChild(newText);
+    wrapperEl.appendChild(newEl);
+  }
+  if (specialCharactersQ === "yes" || specialCharactersQ === "Yes") {
+    var newEl = document.createElement("p");
+    var newText = document.createTextNode("Special Characters selected");
+    newEl.appendChild(newText);
+    wrapperEl.appendChild(newEl);
+  } else {
+    var newEl = document.createElement("p");
+    var newText = document.createTextNode("Special Characters not selected");
+    newEl.appendChild(newText);
+    wrapperEl.appendChild(newEl);
+  }
+}
 
 function writePassword () {
   if (alphabetQ === "yes" || alphabetQ === "Yes") {
@@ -24,23 +78,26 @@ function writePassword () {
   }
   if (bigAlphabetQ === "yes" || bigAlphabetQ === "Yes") {
     questionList = questionList + bigAlphabet;
-  }
-  if (numbersQ === "yes" || numbersQ === "Yes") {
-    questionList = questionList + numbers;
-  }
-  if (specialCharactersQ === "yes" || specialCharactersQ === "Yes") {
-    questionList = questionList + specialCharacters;
-  } 
-  if ( numberDesiredNumeric > 7 && numberDesiredNumeric < 127) {
-    var passGen = "";
-    for (let index = 0; index < numberDesiredNumeric; index++) {
-      var rando = Math.floor(Math.random() * questionList.length);
+    }
+    if (numbersQ === "yes" || numbersQ === "Yes") {
+      questionList = questionList + numbers;
+    }
+    if (specialCharactersQ === "yes" || specialCharactersQ === "Yes") {
+      questionList = questionList + specialCharacters;
+    } 
+    if ( numberDesiredNumeric > 7 && numberDesiredNumeric < 127) {
+      var passGen = "";
+      for (let index = 0; index < numberDesiredNumeric; index++) {
+        var rando = Math.floor(Math.random() * questionList.length);
       passGen = passGen + questionList[rando];
     }
   console.log(passGen);
   passwordText.value = passGen;
+  } else {
+    passwordText.value = "you didn't enter a correct number. please refresh and try again";
   }
 
 }
 
 generateBtn.addEventListener("click", writePassword);
+selectionAppender();
